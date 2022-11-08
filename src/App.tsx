5368c67298ff3acd03e09859ec0363842233191b
b-tsui <btsui.dev@gmail.com>
2022-11-07 23:38:40 -0800

    show comparison + save query url state

--- src/App.tsx
+++ src/App.tsx
@@ -72,7 +72,7 @@ export function App() {
   }
 
   return (
-    <InstantSearch searchClient={searchClient} indexName={indexName} routing={routing}>
+    <InstantSearch searchClient={searchClient} indexName={indexName} routing={true}>
       <header className="header" ref={headerRef}>
         <p className="header-logo">
           <AlgoliaSvg />
@@ -265,30 +265,31 @@ function Hit({ hit }) {
         </p>
         <p className="hit-description">Crystallinity: {hit.crystallinity || "unavailable"}</p>
 
-        <footer>
+        <div>
           <p>
             <span className="hit-em"></span>Band Gap: {formatNumber(hit.band_gap)}{" "}
-            {/* <span className="hit-em hit-rating">
-              <svg xmlns="http://www.w3.org/2000/svg" width="8" height="8" viewBox="0 0 16 16">
-                <path
-                  fill="#e2a400"
-                  fillRule="evenodd"
-                  d="M10.472 5.008L16 5.816l-4 3.896.944 5.504L8 12.616l-4.944 2.6L4 9.712 0 5.816l5.528-.808L8 0z"
-                />
-              </svg>{" "}
-              {hit.rating}
-            </span> */}
+          </p>
+          <p>
+            <span className="hit-em"></span>Volume:{" "}
+            {hit.materials.map(
+              (mat, i) =>
+                `${mat.volume?.toFixed(4) || mat.attributes._odbx_cell_volume?.toFixed(4)} ${
+                  i < hit.n_materials - 1 ? "| " : ""
+                }`
+            )}
           </p>
           <p className="hit-em">
             src:{" "}
-            {hit.url.map((url, i) => (
-              <a href={url} target="_blank" rel="noopener noreferrer" style={{ textDecoration: "none" }}>
-                {hit.source[i]}
-                {i < hit.url.length - 1 && ","}{" "}
-              </a>
-            ))}
+            <div>
+              {hit.url.map((url, i) => (
+                <a href={url} target="_blank" rel="noopener noreferrer" style={{ textDecoration: "none" }}>
+                  {hit.source[i]}
+                  {i < hit.url.length - 1 && " |"}{" "}
+                </a>
+              ))}
+            </div>
           </p>
-        </footer>
+        </div>
       </div>
     </article>
   );
